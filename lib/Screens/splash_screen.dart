7afc4086f1a3c6463b2b806b589c00443ac53ad5
mehrnegarr94 <79174821+mehrnegarr94@users.noreply.github.com>
2023-12-14 13:24:54 +0330

    Update splash_screen.dart

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   // const SplashScreen({ Key key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text(
-        'Classroom Management',
-        style: TextStyle(fontSize: 40),
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Classroom Management',
+          style: TextStyle(fontSize: 40),
+        ),
       ),
-    ));
+    );
   }
 }
