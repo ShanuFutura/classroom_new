a3f2dcbfc2122297354120a19507eec61967b6f8
mehrnegarr94 <79174821+mehrnegarr94@users.noreply.github.com>
2023-12-14 16:59:03 +0330

    Update meetings.dart

--- lib/Widgets/meetings.dart
+++ lib/Widgets/meetings.dart
@@ -1,7 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:school_management/Screens/Exam/constant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -9,12 +9,14 @@ import 'package:url_launcher/url_launcher.dart';
 // import 'package:pet_spotter/utils/constant.dart';
 
 class Meetings extends StatelessWidget {
+  const Meetings({super.key});
+
   // const DoctorsView({Key? key}) : super(key: key);
 
   Future<dynamic> getData() async {
     final spref = await SharedPreferences.getInstance();
     final res = await http.post(
-        Uri.parse(Constants.x + 'view_online_class.php'),
+        Uri.parse('${Constants.x}view_online_class.php'),
         body: {'department_id': spref.getString('department_id')});
     print(res.body);
     return jsonDecode(res.body);
@@ -25,16 +27,16 @@ class Meetings extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.black,
-        title: Text('Online Classes'),
+        title: const Text('Online Classes'),
         backgroundColor: Colors.white,
       ),
       body: FutureBuilder(
           future: getData(),
           builder: (context, snap) {
             if (snap.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
+              return const Center(child: CircularProgressIndicator());
             } else if (snap.data == null) {
-              return Center(
+              return const Center(
                 child: Text('No data'),
               );
             } else {
@@ -48,11 +50,16 @@ class Meetings extends StatelessWidget {
                               '${(snap.data as dynamic)[index]['link']}'));
                         },
                         leading: Image.network(
-                            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/YouTube_social_white_squircle.svg/1200px-YouTube_social_white_squircle.svg.png'),
-                        title: Text((snap.data as dynamic)[index]['heading']),
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/YouTube_social_white_squircle.svg/1200px-YouTube_social_white_squircle.svg.png',
+                        ),
+                        title: Text(
+                          (snap.data as dynamic)[index]['heading'],
+                        ),
                         subtitle: Text(
                           (snap.data as dynamic)[index]['link'],
-                          style: TextStyle(color: Colors.blue),
+                          style: const TextStyle(
+                            color: Colors.blue,
+                          ),
                         ),
                       ),
                     );
