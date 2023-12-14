a406f5705c20af398e50f17b30663aeb01107951
mehrnegarr94 <79174821+mehrnegarr94@users.noreply.github.com>
2023-12-14 13:02:55 +0330

    Update update_profile.dart

--- lib/Screens/Exam/update_profile.dart
+++ lib/Screens/Exam/update_profile.dart
@@ -1,12 +1,12 @@
-import 'dart:convert';
+// ignore_for_file: must_be_immutable, non_constant_identifier_names, prefer_typing_uninitialized_variables, avoid_print
+
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:school_management/Screens/Exam/constant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Update extends StatelessWidget {
-  Update({Key key}) : super(key: key);
+  Update({super.key});
 
   var reg_no;
   var email;
@@ -16,27 +16,25 @@ class Update extends StatelessWidget {
 
   getData() async {
     final pref = await SharedPreferences.getInstance();
-    nameController.text = pref.getString('name');
-    departmentController.text = pref.getString('department');
-    emailController.text = pref.getString('email');
-    mobileController.text = pref.getString('mobile');
+    nameController.text = pref.getString('name')!;
+    departmentController.text = pref.getString('department')!;
+    emailController.text = pref.getString('email')!;
+    mobileController.text = pref.getString('mobile')!;
     reg_no = pref.getString('reg_no');
   }
 
   sendData() async {
-    http.Response result = await http.post(
-        Uri.parse("${Constants.x}update_student.php"),
-        body: {
-          'reg_no': reg_no,
-          'name': nameController.text,
-          'department': departmentController.text,
-          'email': emailController.text,
-          'mobile': mobileController.text,
-        });
+    http.Response result =
+        await http.post(Uri.parse("${Constants.x}update_student.php"), body: {
+      'reg_no': reg_no,
+      'name': nameController.text,
+      'department': departmentController.text,
+      'email': emailController.text,
+      'mobile': mobileController.text,
+    });
     var body = result.body;
 
     print(body);
-    var data = jsonDecode(body);
   }
 
   final nameController = TextEditingController();
@@ -49,8 +47,8 @@ class Update extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('Profile'),
-        backgroundColor: Color.fromARGB(255, 108, 147, 237),
+        title: const Text('Profile'),
+        backgroundColor: const Color.fromARGB(255, 108, 147, 237),
         centerTitle: true,
       ), //AppBar
       body: Center(
@@ -59,12 +57,12 @@ class Update extends StatelessWidget {
             future: getData(),
             builder: (context, snap) {
               if (snap.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
+                return const CircularProgressIndicator();
               } else {
                 return Card(
                   elevation: 50,
                   shadowColor: Colors.black,
-                  color: Color.fromARGB(255, 220, 231, 252),
+                  color: const Color.fromARGB(255, 220, 231, 252),
                   child: SizedBox(
                     width: 300,
                     height: 500,
@@ -74,30 +72,30 @@ class Update extends StatelessWidget {
                         children: [
                           Card(
                             child: ListTile(
-                              title: Text('Register No :'),
+                              title: const Text('Register No :'),
                               trailing: Text(reg_no),
                             ),
                           ),
-                          Divider(),
+                          const Divider(),
 
                           // title: Text(' Name :'),
                           // trailing: Text(snap.data['name']),
                           TextField(
                               controller: nameController,
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                   border: UnderlineInputBorder(),
                                   label: Text('Name'))),
 
-                          Divider(),
+                          const Divider(),
                           TextField(
                               controller: mobileController,
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                   border: UnderlineInputBorder(),
                                   label: Text('Mobile'))),
                           // title: Text(' Email :'),
                           // trailing: Text(snap.data['email']),
 
-                          Divider(),
+                          const Divider(),
                           // TextField(
                           //     controller: departmentController,
                           //     decoration: InputDecoration(
@@ -109,19 +107,19 @@ class Update extends StatelessWidget {
                           // Divider(),
                           TextField(
                               controller: emailController,
-                              decoration: InputDecoration(
+                              decoration: const InputDecoration(
                                   border: UnderlineInputBorder(),
                                   label: Text(
                                       'Email'))), // title: Text('Mobile No :'),
                           // trailing: Text(snap.data['mobile']),
 
-                          Divider(),
+                          const Divider(),
                           FloatingActionButton(
                             onPressed: () {
                               sendData();
                               Navigator.pop(context);
                             },
-                            child: Icon(Icons.save),
+                            child: const Icon(Icons.save),
                           )
                         ],
                       ), //Column
